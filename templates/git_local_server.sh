4cd4016bf0134e23eaebfe81ba7b85e7f9339437
Birgitta B <bboeckel@thoughtworks.com>
2017-07-30 14:41:17 +0200

    Make utility git script more generic
    Relative paths, name of repository determined from dir name

--- templates/git_local_server.sh
+++ templates/git_local_server.sh
@@ -3,8 +3,9 @@ set -e
 
 # A crude little script to push things to our test repository without messing up the local ssh config with temporary keys
 
-! git remote add origin ssh://git@0.0.0.0:2222/git-server/repos/myrepo.git
+repoName=$(basename "$PWD")
+! git remote add origin ssh://git@0.0.0.0:2222/git-server/repos/$repoName.git
 
 gitCmd=$1
 
-ssh-agent bash -c "ssh-add /Users/Shared/projects/build_monitors/gocd-test-env/environment/git-server/keys/id_rsa_gocd_env; ${gitCmd}"
+ssh-agent bash -c "ssh-add ../keys/id_rsa_gocd_env; ${gitCmd}"
